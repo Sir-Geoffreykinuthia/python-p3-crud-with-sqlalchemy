baac4fedd222472e294526410cfe99c0bbb42ee7
geoffrey kinuthia <geoffreykinuthia10@gmail.com>
2023-06-11 13:22:40 +0300

    Done

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -12,6 +12,34 @@ Base = declarative_base()
 
 class Student(Base):
     __tablename__ = 'students'
+    __table_args__ = (
+        # assingning primary ky status to a Column
+        PrimaryKeyConstraint(
+          'id', name='id_pk'),
+        # checking new records to ensure that they do no match existing records at unique Column(s)  
+        UniqueConstraint(
+            'email', name='unique_email'),
+        # here the checkconstraint uses SQL statements to check if new values meet specific criteria 
+        CheckConstraint(
+            'grade BETWEEN 1 AND 12')
+    )
+    # the new constraints for the student model enures that id is a primary key
+    # email is unique and grade is between 1-12
+
+    Index('index_name', 'name')
+    
+    id = Column(Integer())
+    name =Column(String())
+    email = Column(String(55)) 
+    grade = Column(String())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default=datetime.now())
+
+    def __repr__(self):
+        return f"student {self.id}: "\
+        +f"{self.name}, " \
+        +f"Grade {self.grade}"   
+        
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
@@ -24,3 +52,18 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     # this Session class creates the session object 
     session = Session()
+
+    geoffrey_kinuthia =Student(
+        name = "Geoffrey Kinuthia",
+        email = "geoffrey@kinuthia.moringa",
+        grade = 45,
+        birthday =datetime(
+            year = 1889,
+            month = 2,
+            day=14
+        ),
+    )
+    session.add(geoffrey_kinuthia)
+    session.commit()
+
+    print(f"Nw student Id is {geoffrey_kinuthia}. ")
